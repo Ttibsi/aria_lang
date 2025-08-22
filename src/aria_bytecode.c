8262ce936082ec9f2234f1a59073e1fb4e4d2825
ttibsi <ashisbitt@icloud.com>
2025-08-22 21:22:24 +0100

    prep for bytecode

---
+++ src/aria_bytecode.c
@@ -0,0 +1,6 @@
+#include "aria_bytecode.h"
+#include "aria_stack.h"
+
+void bytecode_generation(const Expression expr) {
+    Stack* stack = createStack(64);
+}
---
+++ src/aria_bytecode.h
@@ -0,0 +1,8 @@
+#ifndef ARIA_BYTECODE_H
+#define ARIA_BYTECODE_H
+
+#include "aria_parser.h"
+
+void bytecode_generation(const Expression expr);
+
+#endif // ARIA_BYTECODE_H
