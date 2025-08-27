79c4256e004621d8c1a44a3907798e3370de024d
ttibsi <ashisbitt@icloud.com>
2025-08-27 22:17:36 +0100

    more lexer test stubs!

--- tests/test_lexer.h
+++ tests/test_lexer.h
@@ -1,8 +1,18 @@
 #include "onetest.h"
 #include "aria_lexer.h"
 
-static int test_match(void) { return 1; }
+static int test_peek(void) { return 1; }
+static int test_peekNext(void) { return 1; }
+static int test_advanceChar(void) { return 1; }
+static int test_skipWhitespace(void) { return 1; }
+static int test_makeToken(void) { return 1; }
+static int test_scanEqualVariant(void) { return 1; }
+static int test_scanStringLiteral(void) { return 1; }
+static int test_scanNumber(void) { return 1; }
+static int test_scanIdentifier(void) { return 1; }
 static int test_scanToken(void) { return 1; }
 static int test_advance(void) { return 1; }
+static int test_check(void) { return 1; }
+static int test_match(void) { return 1; }
 static int test_getTokenNumber(void) { return 1; }
 static int test_getTokenChar(void) { return 1; }
