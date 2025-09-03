d453b38b7757fda169bd03e659b6a0f0216cc1ce
ttibsi <ashisbitt@icloud.com>
2025-09-03 21:06:03 +0100

    maybe an initial implementation

--- src/aria_hash.h
+++ src/aria_hash.h
@@ -15,18 +15,18 @@ typedef struct _hash_entry {
 } Entry;
 
 typedef struct {
-    Entry items[MAPSIZE];
+    Entry* items[MAPSIZE];
 } Map;
 
-static Map* mapCreate(const int size);
-static void mapInsert(Map* map, const Entry* entry);
-static void mapRemove(Map* map, const Entry* entry);
+static Map* mapCreate();
+static void mapInsert(Map* map, char* key, int value);
+static void mapRemove(Map* map, char* key);
 static int mapFind(Map* map, const char* key);
 
 #ifdef ARIA_HASH_IMPLEMENTATION
-static Map* mapCreate(const int size) {
+static Map* mapCreate() {
     Map* map = (Map*)malloc(sizeof(Map) * size);
-    for (size_t i = 0; i < MAPSIZE; i++) { map->items[i] = (Entry){NULL, NULL, NULL, 0}; }
+    for (size_t i = 0; i < MAPSIZE; i++) { map->items[i] = (Entry*)malloc(sizeof(Entry)); }
     return map;
 }
 
@@ -41,16 +41,49 @@ static uint32_t hash(const char* key) {
     return hash % MAPSIZE;
 }
 
-static void mapInsert(Map* map, const Entry* entry) {
+static void mapInsert(Map* map, char* key, int value) {
+    int hash_num = hash(key);
+    Entry* current = map->items[hash_num];
+    while (current->next != NULL) { current = current->next; }
+
+    current->next = (Entry*)malloc(sizeof(Entry));
+    current->next->prev = current;
+    current->next->next = NULL;
+    current->key = key;
+    current->value = value;
 }
 
-static void mapRemove(Map* map, const Entry* entry) {
+static void mapRemove(Map* map, char* key) {
+    int hash_num = hash(key);
+    Entry* current = map->items[hash_num];
+    while (strcmp(current->key, key) != 0) { current = current->next; }
+
+    current->next->prev = current->prev;
+    current->prev->next = current->next;
+    free(current);
 }
 
 static int mapFind(Map* map, const char* key) {
+    int hash_num = hash(key);
+    Entry* current = map->items[hash_num];
+    while (strcmp(current->key, key) != 0) { current = current->next; }
+    return current->value;
 }
 
 static void mapFree(Map* map) {
+    for (int i = 0; i < MAPSIZE; i++) {
+        Entry* current = map->items[i];
+        while (current->next != NULL) { current = current->next; }
+
+        if (current->prev != NULL) {
+            current = current->prev;
+        }
+
+        while (current->prev != NULL) {
+            free(current->next);
+            current = current->prev;
+        }
+    }
     free(map);
 }
 
