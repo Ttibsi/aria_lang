52553b93a3845e915cb0d465232ef05e6ae58433
Ttibsi <ashisbitt@icloud.com>
2025-09-29 13:54:30 +0000

    Add test stubs to runner

--- tests/test.c
+++ tests/test.c
@@ -2,12 +2,13 @@
 #include "onetest.h"
 
 #include "test_buffer.h"
-// #include "test_bytecode.h"
+#include "test_hash.h"
 #include "test_lexer.h"
+#include "test_parser.h"
+// #include "test_bytecode.h"
 // #include "test_stack.h"
 // #include "test_executor.h"
 // #include "test_interpreter.h"
-#include "test_hash.h"
 
 int main(void) {
     onetest_test_t tests[] = {
@@ -16,11 +17,15 @@ int main(void) {
         ONETEST_TEST(test_bufferInsert),
         ONETEST_TEST(test_bufferGet),
         ONETEST_TEST(test_bufferPeek),
-        // test_bytecode.h
-        // ONETEST_TEST(test_handleOperation),
-        // ONETEST_TEST(test_handleAtom),
-        // ONETEST_TEST(test_nextInst),
-        // ONETEST_TEST(test_bytecodeGeneration),
+
+        // test_hash.h
+        ONETEST_TEST(test_mapCreate),
+        ONETEST_TEST(test_hash),
+        ONETEST_TEST(test_mapInsert),
+        ONETEST_TEST(test_mapRemove),
+        ONETEST_TEST(test_mapExists),
+        ONETEST_TEST(test_mapFind),
+
         // test_lexer.h
         ONETEST_TEST(test_peek),
         ONETEST_TEST(test_peekNext),
@@ -37,6 +42,30 @@ int main(void) {
         ONETEST_TEST(test_match),
         ONETEST_TEST(test_getTokenNumber),
         ONETEST_TEST(test_getTokenChar),
+
+        // test_parser.h
+        ONETEST_TEST(test_parseConst),
+        ONETEST_TEST(test_parseFor),
+        ONETEST_TEST(test_parseIf),
+        ONETEST_TEST(test_parsePrint),
+        ONETEST_TEST(test_parseReturn),
+        ONETEST_TEST(test_parseSwitch),
+        ONETEST_TEST(test_parseVar),
+        ONETEST_TEST(test_parseIdentifier),
+        ONETEST_TEST(test_parseExpression),
+        ONETEST_TEST(test_parseClass),
+        ONETEST_TEST(test_parseExport),
+        ONETEST_TEST(test_parseBlock),
+        ONETEST_TEST(test_parseFunc),
+        ONETEST_TEST(test_parseImport),
+        ONETEST_TEST(test_ariaParse),
+        ONETEST_TEST(test_createNode),
+
+        // test_bytecode.h
+        // ONETEST_TEST(test_handleOperation),
+        // ONETEST_TEST(test_handleAtom),
+        // ONETEST_TEST(test_nextInst),
+        // ONETEST_TEST(test_bytecodeGeneration),
         // ONETEST_TEST(test_isFull),
         // ONETEST_TEST(test_isEmpty),
         // ONETEST_TEST(test_stackPush),
@@ -50,13 +79,6 @@ int main(void) {
         // ONETEST_TEST(test_division),
         // ONETEST_TEST(test_brackets),
         // ONETEST_TEST(test_printing),
-        // test_hash.h
-        ONETEST_TEST(test_mapCreate),
-        ONETEST_TEST(test_hash),
-        ONETEST_TEST(test_mapInsert),
-        ONETEST_TEST(test_mapRemove),
-        ONETEST_TEST(test_mapExists),
-        ONETEST_TEST(test_mapFind),
     };
 
     return onetest_exec(tests, sizeof(tests) / sizeof(tests[0]));
