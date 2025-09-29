a7f5f34c954fc15dd5b28009d23e7dac998c6a32
ttibsi <ashisbitt@icloud.com>
2025-09-29 21:56:33 +0100

    test parsereturn

--- tests/test_parser.h
+++ tests/test_parser.h
@@ -1,11 +1,35 @@
 #include "onetest.h"
+
+#include "aria_lexer.h"
 #include "aria_parser.h"
+
+#define CREATE_LEXER(src) \
+Aria_Lexer l = (Aria_Lexer){ \
+        .source = src, \
+        .pc = 0, \
+        .tokens = bufferCreate(sizeof(Aria_Token), 8), \
+        .buf_index = 0 \
+    };
 
 static int test_parseConst(void) { return 1; }
 static int test_parseFor(void) { return 1; }
 static int test_parseIf(void) { return 1; }
 static int test_parsePrint(void) { return 1; }
-static int test_parseReturn(void) { return 1; }
+static int test_parseReturn(void) {
+    CREATE_LEXER("return 69;");
+
+    Aria_Token tok = (Aria_Token){true, TOK_RETURN, 0, 6};
+    bufferInsert(&l.tokens, &tok);
+
+    tok = (Aria_Token){true, TOK_NUMBER, 7, 2};
+    bufferInsert(&l.tokens, &tok);
+
+    ASTNode node = parseReturn(&l);
+    onetest_assert(node.type == AST_VALUE);
+    onetest_assert(node.value == 69);
+
+    return 0;
+}
 static int test_parseSwitch(void) { return 1; }
 static int test_parseVar(void) { return 1; }
 static int test_parseIdentifier(void) { return 1; }
