bba2080b6ca67e2c62e58a7907b46e175321badc
Ttibsi <ashisbitt@icloud.com>
2025-10-07 15:00:32 +0000

    final test

--- tests/test_buffer.h
+++ tests/test_buffer.h
@@ -53,4 +53,29 @@ static int test_bufferPeek(void) {
     return 0;
 }
 
-static int test_bufferCopy(void) { return 1; }
+static int test_bufferCopy(void) {
+    Aria_Buffer *src = bufferCreate(sizeof(int), 8);
+    int vals[] = {10, 20, 30};
+    for (size_t i = 0; i < sizeof(vals)/sizeof(vals[0]); ++i) {
+        int *p = (int *)malloc(sizeof(int));
+        *p = vals[i];
+        bufferInsert(src, p);
+    }
+
+    Aria_Buffer *dest = bufferCreate(sizeof(int), 4);   /* capacity will be overwritten */
+    bufferCopy(dest, src);
+
+    onetest_assert(dest->elem_size == src->elem_size);
+    onetest_assert(dest->capacity  == src->capacity);
+    onetest_assert(dest->size      == src->size);
+
+    for (size_t i = 0; i < src->size; ++i) {
+        int src_val = *(int *)bufferGet(src, i);
+        int dest_val = *(int *)bufferGet(dest, i);
+        onetest_assert(src_val == dest_val);
+    }
+
+    bufferFree(src);
+    bufferFree(dest);
+    return 0;
+}
