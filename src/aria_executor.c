23e20303894c9ffdd46ef68d18cfa407d5cb0703
Ttibsi <ashisbitt@icloud.com>
2025-10-09 07:57:33 +0000

    Handle executing basic bytecode

--- src/aria_executor.c
+++ src/aria_executor.c
@@ -1,47 +1,56 @@
-#if 0
 #include <stddef.h>
 #include <stdio.h>
 
+#define ARIA_STACK_IMPL
 #include "aria_executor.h"
 
-Stack* ariaExecute(Bytecode* bc) {
+#define ARIA_HASH_IMPL
+#include "aria_hash.h"
+
+#define ARIA_BUFFER_IMPL
+#include "aria_buffer.h"
+
+// Map<str, Aria_Chunk>
+static Map* dispatch_table = NULL;
+
+void executeInstruction(Stack* global, Stack* local, Aria_Bytecode* inst) {
+    switch (inst->op) {
+        case OP_STORE_CONST:
+            stackPush(local, inst->operand);
+            break;
+        case OP_NULL:
+          break;
+    }
+
+    return;
+}
+
+int executeFunction(Stack* global_stack, Aria_Chunk* func) {
+    Stack* local_stack = createStack(1024);
+
+    Aria_Bytecode* playhead = func->buf;
+    while (playhead != NULL) {
+        executeInstruction(global_stack, local_stack, playhead);
+        playhead = playhead->next;
+    }
+
+    return stackPeek(local_stack);
+}
+
+Stack* ariaExecute(Aria_Module* mod) {
     Stack* stack = createStack(1024);
+    dispatch_table = mapCreate(sizeof(Aria_Chunk));
 
-    do {
-        switch (bc->inst) {
-            case INST_LOAD_CONST:
-                stackPush(stack, bc->value);
-                break;
-            case INST_ADD: {
-                    int b = stackPop(stack);
-                    int a = stackPop(stack);
-                    stackPush(stack, a + b);
-                } break;
-            case INST_SUB: {
-                    int b = stackPop(stack);
-                    int a = stackPop(stack);
-                    stackPush(stack, a - b);
-                } break;
-            case INST_MUL: {
-                    int b = stackPop(stack);
-                    int a = stackPop(stack);
-                    stackPush(stack, a*b);
-                } break;
-            case INST_DIV: {
-                    int b = stackPop(stack);
-                    int a = stackPop(stack);
-                    stackPush(stack, a/b);
-                } break;
-            case INST_PRINT:
-                printf("%d\n", bc->value);
-                break;
-            case INST_NULL:
-                break;
-            }
+    for (size_t i = 0; i < mod->buf->size; i++) {
+        Aria_Chunk* chunk = (Aria_Chunk*)bufferGet(mod->buf, i);
+        int ret = mapInsert(dispatch_table, chunk->name, chunk);
+        if (ret) {
+            // do something here to show multiple definitions for the same function
+        }
+    }
 
-        bc = bc->next;
-    } while (bc != NULL);
+    Aria_Chunk* main = mapFind(dispatch_table, "main");
+    stackPush(stack, executeFunction(stack, main));
 
     return stack;
 }
-#endif
