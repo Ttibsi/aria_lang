917fa156f5c30838d5abea63022b73889d190442
ttibsi <ashisbitt@icloud.com>
2025-10-15 21:02:33 +0100

    partial resolve of bytecode introduction

--- src/aria/aria.c
+++ src/aria/aria.c
@@ -3,18 +3,30 @@
 
 #include "aria_lexer.h"
 #include "aria_parser.h"
+#include "aria_bytecode.h"
 
 [[nodiscard]] char* ariaVersion() {
     return "0.0.0";
 }
 
-void ariaFree(Aria_Lexer* L, ASTNode* ast) {
+void ariaFree(Aria_Lexer* L, ASTNode* ast, Aria_Module* mod) {
     // Lexer
     free(L->source);
     bufferFree(L->tokens);
     L = NULL;
 
+    // Parser
     nodeFree(*ast);
+    ast = NULL;
+
+    // Bytecode
+    for (size_t i = 0; i < mod->buf->size; ++i) {
+        Aria_Chunk *c = bufferGet(mod->buf, i);
+        freeChunk(c);
+    }
+    bufferFree(mod->buf);
+    free(mod);
+    mod = NULL;
 }
 
 [[nodiscard]] int ariaInterpret(char* text, const bool debug_mode) {
@@ -25,6 +37,9 @@ void ariaFree(Aria_Lexer* L, ASTNode* ast) {
     ASTNode ast = ariaParse(&L);
     if (debug_mode) { printAST(ast, &L); }
 
-    ariaFree(&L, &ast);
+    Aria_Module* main_mod = ariaCompile(&ast);
+    if (debug_mode) { printBytecode(main_mod); }
+
+    ariaFree(&L, &ast, main_mod);
     return 0;
 }
