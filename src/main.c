eb18435c609bcf623fff36536d301432e27844ed
Ttibsi <ashisbitt@icloud.com>
2026-01-14 08:05:47 +0000

    ensure pre-commit passes

--- src/main.c
+++ src/main.c
@@ -3,6 +3,4 @@
 #include "mylib/t.h"
 #include "other/u.h"
 
-int main() {
-    printf("Hello world: %d, %d\n", add(34, 35), mul(5));
-}
+int main() { printf("Hello world: %d, %d\n", add(34, 35), mul(5)); }
